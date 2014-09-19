535088305c95defa7fc0e648641fdced406256fa
Ray Gauss <ray.gauss@alfresco.com>
2014-09-19 08:59:05 +0000

    ACE-2800: Licensing for Alfresco Services Repository Dependencies and Extensions
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/services/alfresco-components/HEAD/alfresco-repo-dependencies/alfresco-events@84692 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- src/test/java/org/alfresco/events/test/TestDataItem.java
+++ src/test/java/org/alfresco/events/test/TestDataItem.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2014 Alfresco Software, Ltd.  All rights reserved.
+ *
+ * License rights for this program may be obtained from Alfresco Software, Ltd. 
+ * pursuant to a written agreement and any use of this program without such an 
+ * agreement is prohibited. 
+ */
 package org.alfresco.events.test;
 
 import static org.junit.Assert.assertEquals;
